07de747509a03823448488dc9e6cf30825816e31
Marcinho Coutinho <marciojscoutinho@gmail.com>
2023-02-01 14:04:42 -0300

    Jogo da velha

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './styles.css';
+
+import Square from './Square'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div>
+    <Square />
+    </div>
+  </React.StrictMode>,
+)
+
